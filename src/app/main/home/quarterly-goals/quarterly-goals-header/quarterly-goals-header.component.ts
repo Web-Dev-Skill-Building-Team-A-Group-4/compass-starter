5e0c76c0a417969f4356421876ba276556aac466
aluo28 <aluo28@ucsc.edu>
2026-07-16 18:17:58 -0700

    feat: implement Quarterly Goals Header

--- src/app/main/home/quarterly-goals/quarterly-goals-header/quarterly-goals-header.component.ts
+++ src/app/main/home/quarterly-goals/quarterly-goals-header/quarterly-goals-header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, ChangeDetectionStrategy, input, output, inject, WritableSignal, Signal, signal, computed, Inject, Injector } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, input, output, inject, WritableSignal, Signal, signal, computed, Inject, Injector, OutputEmitterRef } from '@angular/core';
 import { QuarterlyGoalsHeaderAnimations } from './quarterly-goals-header.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { getQuarterAndYear } from '../../../../core/utils/time.utils';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-quarterly-goals-header',
   templateUrl: './quarterly-goals-header.component.html',
   styleUrls: ['./quarterly-goals-header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: QuarterlyGoalsHeaderAnimations,
+  changeDetection: ChangeDetectionStrategy.OnPush, 
+  animations: QuarterlyGoalsHeaderAnimations, 
   standalone: true,
   imports: [
   ],
@@ -17,22 +19,28 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
 export class QuarterlyGoalsHeaderComponent implements OnInit {
   readonly authStore = inject(AuthStore);
   // --------------- INPUTS AND OUTPUTS ------------------
-
   /** The current signed in user. */
   currentUser: Signal<User> = this.authStore.user;
 
-  // --------------- LOCAL UI STATE ----------------------
+  editClicked = output<boolean>();
 
+  // --------------- LOCAL UI STATE ----------------------
   /** Loading icon. */
   loading: WritableSignal<boolean> = signal(false);
+  
+  getQuarterAndYear = getQuarterAndYear;
 
   // --------------- COMPUTED DATA -----------------------
 
   // --------------- EVENT HANDLING ----------------------
+  editGoal(){
+    this.editClicked.emit(true);
+  }
 
   // --------------- OTHER -------------------------------
 
   constructor(
+    private snackBar: MatSnackBar,
     private injector: Injector,
     @Inject(BATCH_WRITE_SERVICE) private batch: BatchWriteService,
   ) { }
--- src/app/main/home/quarterly-goals/quarterly-goals.component.ts
+++ src/app/main/home/quarterly-goals/quarterly-goals.component.ts
@@ -3,6 +3,9 @@ import { QuarterlyGoalsAnimations } from './quarterly-goals.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { QuarterlyGoalsHeaderComponent } from './quarterly-goals-header/quarterly-goals-header.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { QuarterlyGoalsModalComponent } from './quarterly-goals-modal/quarterly-goals-modal.component';
 
 @Component({
   selector: 'app-quarterly-goals',
@@ -12,6 +15,7 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
   animations: QuarterlyGoalsAnimations,
   standalone: true,
   imports: [
+    QuarterlyGoalsHeaderComponent,
   ],
 })
 export class QuarterlyGoalsComponent implements OnInit {
@@ -26,13 +30,24 @@ export class QuarterlyGoalsComponent implements OnInit {
   /** Loading icon. */
   loading: WritableSignal<boolean> = signal(false);
 
+  /** For storing the dialogRef in the opened modal. */
+  dialogRef: MatDialogRef<any>;
+
   // --------------- COMPUTED DATA -----------------------
 
   // --------------- EVENT HANDLING ----------------------
+  openModal(editClicked: boolean) {
+    this.dialogRef = this.dialog.open(QuarterlyGoalsModalComponent, {
+      height: '90%',
+      position: { bottom: '0' },
+      panelClass: 'goal-modal-panel',
+    });
+  }
 
   // --------------- OTHER -------------------------------
 
   constructor(
+    private dialog: MatDialog,
     private injector: Injector,
     @Inject(BATCH_WRITE_SERVICE) private batch: BatchWriteService,
   ) { }
