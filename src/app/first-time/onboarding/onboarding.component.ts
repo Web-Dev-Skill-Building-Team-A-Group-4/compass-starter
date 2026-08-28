ed64e06f8fff307885dc8658919d2dcc539044bc
Alexis Luo <goldenpoodle2007@gmail.com>
2026-08-28 11:55:17 -0700

    fix: use switch statements for onboarding pages

--- src/app/first-time/onboarding/onboarding.component.ts
+++ src/app/first-time/onboarding/onboarding.component.ts
@@ -3,7 +3,13 @@ import { OnboardingAnimations } from './onboarding.animations';
 import { User, OnboardingState } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { InitialPageComponent } from './step-pages/initial-page/initial-page.component';
+import { OnboardLongTermGoalsComponent } from './step-pages/onboard-long-term-goals/onboard-long-term-goals.component';
+import { OnboardLongTermTransitionComponent } from './step-pages/onboard-long-term-transition/onboard-long-term-transition.component';
 import { OnboardQuarterlyGoalsComponent } from './step-pages/onboard-quarterly-goals/onboard-quarterly-goals.component';
+import { OrganizeQuarterlyGoalsComponent } from './step-pages/organize-quarterly-goals/organize-quarterly-goals.component';
+import { OnboardWeeklyGoalsComponent } from './step-pages/onboard-weekly-goals/onboard-weekly-goals.component';
+import { FinalPageComponent } from './step-pages/final-page/final-page.component';
 
 @Component({
   selector: 'app-onboarding',
@@ -13,7 +19,13 @@ import { OnboardQuarterlyGoalsComponent } from './step-pages/onboard-quarterly-g
   standalone: true,
   animations: OnboardingAnimations,
   imports: [
+    InitialPageComponent,
+    OnboardLongTermGoalsComponent,
+    OnboardLongTermTransitionComponent,
     OnboardQuarterlyGoalsComponent,
+    OrganizeQuarterlyGoalsComponent,
+    OnboardWeeklyGoalsComponent,
+    FinalPageComponent,
   ],
 })
 export class OnboardingComponent implements OnInit {
